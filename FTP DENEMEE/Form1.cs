7f9dff1ef7f3b5c0f072038f83ba53957f971df3
furkanklss <furkankls70@gmail.com>
2023-05-10 23:17:01 +0300

    Add project files.

---
+++ FTP DENEMEE/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace FTP_DENEMEE
+{
+    public partial class Form1 : Form
+    {
+        public string Username;
+        public string Filename;
+        public string Fullname;
+        public string Server;
+        public string Password;
+        public string path;
+        public string localdest;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true, ValidateNames = true, Filter = "All Files|*.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi = new FileInfo(ofd.FileName);
+                    Username = textBox1.Text;
+                    Password = textBox2.Text;
+                    Server = textBox3.Text;
+                    Filename = fi.Name;
+                    Fullname = fi.FullName;
+                }
+
+
+
+            }
+
+            //Upload Method.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, Filename)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(Username, Password);
+            Stream ftpstream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(Fullname);
+
+            // Method to calculate and show the progress.
+            byte[] buffer = new byte[1024];
+            double total = (double)fs.Length;
+            int byteRead = 0;
+            double read = 0;
+            do
+            {
+                byteRead = fs.Read(buffer, 0, 1024);
+                ftpstream.Write(buffer, 0, byteRead);
+                read += (double)byteRead;
+
+                double percentage = read / total * 100;
+                
+            }
+            while (byteRead != 0);
+            fs.Close();
+            ftpstream.Close();
+        }
+
+      
+    }
+}
